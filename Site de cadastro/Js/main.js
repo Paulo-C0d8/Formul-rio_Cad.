e46540db61f448ff3d4027434387c7a4de61829a
Paulo-C0d8 <149203770+Paulo-C0d8@users.noreply.github.com>
2024-01-31 15:16:13 -0300

    Update main.js

--- Site de cadastro/Js/main.js
+++ Site de cadastro/Js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 $('#telefone').mask('(00) 00000-0000')
-$('#cep').mask('_____-___')
-$('#cpf').mask('___.___.___-__')
+$('#cep').mask('00000-000')
+$('#cpf').mask('000.000.000-00')
 
 // Validação
 
